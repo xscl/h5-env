9c30916ff8f193ce1b8e32950bfeedfa7a97eefb
Tingkl <339776223@qq.com>
2016-07-27 17:54:46 +0800

    s

--- server/routes/yangmi.js
+++ server/routes/yangmi.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var router = express.Router();
 var assemble = require('../assemble');
-assemble.assemble(router, 'yangmi');
+assemble.assemble2(router, 'yangmi');
 module.exports = router;
