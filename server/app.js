776fe1fe7a6491ce098e0c5cf2bbf796281bc614
Tingkl <339776223@qq.com>
2016-07-27 17:29:15 +0800

    s

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/dzs', dzs);
-app.use('/yangmi', yangmi);
+//app.use('/yangmi', yangmi);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
